73ef2cdb744ae445bc59e6eb18ca120bddb9a7c2
pbezliapovich <pavel.bezliapovich@sbb.ch>
2024-09-23 12:47:54 +0200

    feat: Extended REST API annotation (#41)
    Refs: #40

--- src/main/java/ch/sbb/polarion/extension/excel_importer/service/ImportResult.java
+++ src/main/java/ch/sbb/polarion/extension/excel_importer/service/ImportResult.java
@@ -1,5 +1,6 @@
 package ch.sbb.polarion.extension.excel_importer.service;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +10,14 @@ import java.util.List;
 @Data
 @Builder
 @AllArgsConstructor
+@Schema(description = "Represents the result of an Excel import operation")
 public class ImportResult {
+    @Schema(description = "List of IDs that were updated during the import process")
     private List<String> updatedIds;
-    private List<String> createdIds;
-    private List<String> unchangedIds;
 
+    @Schema(description = "List of IDs that were created during the import process")
+    private List<String> createdIds;
+
+    @Schema(description = "List of IDs that were unchanged during the import process")
+    private List<String> unchangedIds;
 }
