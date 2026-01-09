bd32f94342f9606117b805d804e76206a9449f9a
Sergey Grigoriev <sergey.grigoriev@sbb.ch>
2026-01-09 08:18:03 +0100

    chore: fix SonarQube codesmells (#165)

--- src/main/java/ch/sbb/polarion/extension/excel_importer/service/ImportJobsCleaner.java
+++ src/main/java/ch/sbb/polarion/extension/excel_importer/service/ImportJobsCleaner.java
@@ -3,29 +3,35 @@ package ch.sbb.polarion.extension.excel_importer.service;
 import ch.sbb.polarion.extension.excel_importer.utils.PropertiesUtility;
 
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public final class ImportJobsCleaner {
 
-    private static Future<?> cleaningJob;
+    private static ScheduledExecutorService executorService;
 
     private ImportJobsCleaner() {
     }
 
     public static synchronized void startCleaningJob() {
-        if (cleaningJob != null) {
+        if (executorService != null) {
             return;
         }
 
         int finishedJobTimeout = new PropertiesUtility().getFinishedJobTimeout();
 
-        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-        cleaningJob = executorService.scheduleWithFixedDelay(
+        executorService = Executors.newSingleThreadScheduledExecutor();
+        executorService.scheduleWithFixedDelay(
                 () -> ImportJobsService.cleanupExpiredJobs(finishedJobTimeout),
                 0,
                 finishedJobTimeout,
                 TimeUnit.MINUTES);
+    }
+
+    public static synchronized void stopCleaningJob() {
+        if (executorService != null) {
+            executorService.shutdown();
+            executorService = null;
+        }
     }
 }
